b5506928536afa589c7c1ee7f1e4d7206d3ca5b0
MartinBulla <bulla.mar@gmail.com>
2026-01-05 18:47:03 +0100

    Update _runRmarkdown.R

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -1,5 +1,5 @@
 require(rmarkdown)
 
-rmarkdown::render('R/zDraft.R', output_dir = 'Output',  output_file = 'MS_draft_v1.html')
+rmarkdown::render('R/MS_v1.R', output_dir = 'Output',  output_file = 'MS_draft_v1.html')
 
 rmarkdown::render('R/Out_Robutstness_check.r', output_dir = 'Output',  output_file = 'Out_Robustness_check.html')
